b6e6f7999620a03b5aa80cd89ada08bdd4a5e219
adamkujawski <kujawski.ad@gmail.com>
2021-02-28 19:24:34 +0100

    adds custom View possibility to get_widgets method
    adds documentation to doc string

--- spectacoular/factory.py
+++ spectacoular/factory.py
@@ -99,43 +99,90 @@ def widget_mapper_factory(obj,traitname,widgetType):
         "mapping for widget type {} does not exist!".format(widgetType))
 
 
-def get_widgets(self): 
-    """
-    Get instances of the widgets defined in :attr:`trait_widget_mapper`. 
+def get_widgets(self, trait_widget_mapper={}, trait_widget_args={}): 
+    """This function creates a View (a bunch of instanciated widgets without a layout) 
+    defined by the :attr:`trait_widget_mapper` mapping attribute
+    of the given object (or specified via :meth:`get_widgets` method arguments). 
     
     This function is implemented in all SpectAcoular classes and is added to
     Acoular's classes in bokehview.py. It builds Bokeh widgets from 
     corresponding class trait attributes by utilizing the :class:`TraitWidgetMapper` 
-    factory. The desired mapping is defined in the :attr:`trait_widget_mapper` 
+    derived classes. The desired mapping is defined by the :attr:`trait_widget_mapper` 
     dictionary. 
-     
+
+    This function implements multiple cases of View construction:
+
+    * (Case 1 - Default View) get_widgets() is called by a BaseSpectacoular 
+    derived instance without specifying trait_widget_mapper and trait_widget_args 
+    as function arguments. 
+    In this case, the mapping defined by the object instance attributes
+    (`self.trait_widget_mapper`,self.`trait_widget_args`) will be used to 
+    construct the View. 
+    
+    * (Case 2 - Custom View (a)) get_widgets() is called by a BaseSpectacoular 
+    derived instance and the desired mapping is given by the 
+    :meth:`get_widgets` function arguments (trait_widget_mapper and trait_widget_args).
+    In this case, the mapping defined by the function arguments will be used to
+    create the View. The instance attributes (`self.trait_widget_mapper`,self.`trait_widget_args`) 
+    will be superseded. 
+    
+    * (Case 3 - No Predefined View) :meth:`get_widgets` is called and a HasTraits derived instance 
+    is given as the first argument to the function. The instance object has no 
+    trait_widget_mapper and trait_widget_args attributes. 
+    In this case, a default mapping is created from all editable traits to create the view. 
+
+    * (Case 4 - Custom View (b)) :meth:`get_widgets` is called and a HasTraits derived instance 
+    is given as the first argument to the function. The instance object has no 
+    trait_widget_mapper and trait_widget_args attributes, but a mapping is defined by the 
+    second (and third) function argument. 
+    In this case, the mapping defined by the function arguments will be used to create the View. 
+    
+    Parameters
+    ----------
+    trait_widget_mapper : dict, optional
+        contains the desired mapping of a variable name (dict key) 
+        to a Bokeh widget type (dict value), by default {}
+    trait_widget_args : dict, optional
+        contains the desired widget kwargs (dict values) for each variable name (dict key),
+         by default {}
 
     Returns
     -------
-    None.
-
+    dict
+        A dictionary containing the variable names as the key and the Bokeh widget
+        instance as value.
     """
 
     widgetdict = {}
-    if hasattr(self,'trait_widget_mapper'):    
-        for (traitname,widgetType) in list(self.trait_widget_mapper.items()):
-            widgetMapper = widget_mapper_factory(self,traitname,widgetType)
-            kwargs = self.trait_widget_args.get(traitname)
-            if kwargs:
-                widgetdict[traitname] = widgetMapper.create_widget(**kwargs)
-            else:
-                widgetdict[traitname] = widgetMapper.create_widget()                                            
-    else:
-        for traitname in self.editable_traits():
-            traittype = self.trait(traitname).trait_type
-            defaultWidgetType = DEFAULT_TRAIT_WIDGET_MAPPINGS.get(traittype.__class__)
-            if defaultWidgetType:
-                widgetMapper = widget_mapper_factory(self,traitname,defaultWidgetType)
-                widgetdict[traitname] = widgetMapper.create_widget()                
-            else:
-                pass #TODO: should we rather raise a warning here?
+    if not hasattr(self,'trait_widget_mapper'): # in case of classes that are not part of Spectacoular
+        if not trait_widget_mapper: # if no trait_widget_mapper defined, take the default
+            for traitname in self.editable_traits():
+                traittype = self.trait(traitname).trait_type
+                defaultWidgetType = DEFAULT_TRAIT_WIDGET_MAPPINGS.get(traittype.__class__)
+                if defaultWidgetType:
+                    widgetMapper = widget_mapper_factory(self,traitname,defaultWidgetType)
+                    widgetdict[traitname] = widgetMapper.create_widget()                
+                else: # in case no default widget exists, a simple TextInput that is readonly will
+                    #be created
+                    widgetMapper = widget_mapper_factory(self,traitname,TextInput)
+                    widgetdict[traitname] = widgetMapper.create_widget(disabled=True)         
+            return widgetdict
+    else: # in case of BaseSpectacoular derived classes
+        # check if a custom view is defined via function arguments trait_widget_mapper
+        # and trait_widget_args
+        if not trait_widget_mapper:
+            trait_widget_mapper = self.trait_widget_mapper
+        if not trait_widget_args:
+            trait_widget_args = self.trait_widget_args                                           
+    # create widgets for spectacoular derived classes
+    for (traitname,widgetType) in list(trait_widget_mapper.items()):
+        widgetMapper = widget_mapper_factory(self,traitname,widgetType)
+        kwargs = trait_widget_args.get(traitname)
+        if kwargs:
+            widgetdict[traitname] = widgetMapper.create_widget(**kwargs)
+        else:
+            widgetdict[traitname] = widgetMapper.create_widget()          
     return widgetdict
-
 
 def set_widgets(self,**kwargs):
     """
