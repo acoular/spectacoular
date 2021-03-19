3e98536a38133200a4828ad794f8352585692392
Simon Jekosch <s.jekosch@tu-berlin.de>
2021-03-19 11:35:53 +0100

    Update start_typhoon.bat to current card serial numbers

--- apps/start_typhoon.bat
+++ apps/start_typhoon.bat
@@ -1,1 +1,1 @@
-bokeh serve --show Measurement_App --args --device=typhoon --blocksize=256 --syncorder 10120 10123 10118 10119 10115 10116 10030 10038 10095 10028 10092 10093 
+bokeh serve --show Measurement_App --args --device=typhoon --blocksize=256 --syncorder 10093 10123 10118 10119 10092 10116 10030 10038 10028 10095 10115 10153
