4e88d405ac71387f181d140a2691062ffae2a5ac
adamkujawski <kujawski.ad@gmail.com>
2021-09-08 11:39:45 +0200

    clean .bat

--- apps/start_typhoon.bat
+++ apps/start_typhoon.bat
@@ -1,5 +1,2 @@
 bokeh serve --show Measurement_App --args --device=typhoon --blocksize=256 --syncorder 10093 10123 10118 10119 10092 10116 10030 10038 10028 10095 10115 10153
-<<<<<<< HEAD
-=======
 
->>>>>>> 0abcf60ca4e1606ce0f853a0f13160b239176747
