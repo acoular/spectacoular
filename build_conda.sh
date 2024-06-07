18e3cd63e5ca2f7b9c5e3d79575e2f98ae552a9e
Adam Kujawski <adam.kujawski@tu-berlin.de>
2024-06-07 20:42:02 +0200

    Merge branch 'github_workflow'

--- build_conda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# building recipe for noarch conda package
-conda build --no-copy-test-source-files -c acoular recipe.local
-
-
-
--- copy_html.sh
+++
@@ -1,6 +0,0 @@
-# copy html content from sphinx documentation to docs folder for githubpages rendering
-rm -r docs/* && \
-cp -r sphinx/_build/html/* docs
-
-
-
