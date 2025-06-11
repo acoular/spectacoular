53c713dcb295f6878d23ed734e974cc06437c349
Adam Kujawski <adam.kujawski@tu-berlin.de>
2025-06-11 13:26:36 +0200

    update post release version

--- spectacoular/version.py
+++ spectacoular/version.py
@@ -2,5 +2,5 @@
 # Copyright (c) Acoular Development Team.
 # ------------------------------------------------------------------------------
 __author__ = 'Acoular Development Team'
-__date__ = '13 May 2025'
-__version__ = '25.05.post1'
+__date__ = '11 June 2025'
+__version__ = '25.05.post2'
