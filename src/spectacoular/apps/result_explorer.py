056fc89dc841ff5bc4b5351b1045b76a0f9a7f7d
Art Pelling <a.pelling@tu-berlin.de>
2026-04-29 12:59:39 +0200

    move apps

--- src/spectacoular/apps/result_explorer.py
+++ src/spectacoular/apps/result_explorer.py
@@ -1,11 +1,10 @@
 import sys
 import subprocess
-import spectacoular
 from pathlib import Path
 
 
 def main():
-    app_dir = Path(spectacoular.__file__).parent.parent / "apps"
+    app_dir = Path(__file__).parent
     command = ["bokeh", "serve", app_dir / "FreqBeamformingExample"]
     command.extend(sys.argv[1:])  # Capture additional arguments like --port 5007)
     # Run the command
