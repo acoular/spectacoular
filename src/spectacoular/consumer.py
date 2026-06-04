54bf226a0b6dfe9ceea50da8abc278e7156f25a1
jtodev <jan@ostendorf.com>
2026-06-04 18:55:44 +0200

    Format consumer

--- src/spectacoular/consumer.py
+++ src/spectacoular/consumer.py
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------------------
 # Copyright (c), Acoular Development Team.
 # ------------------------------------------------------------------------------
-"""Implements consumer classes that provide Acoular data to Bokeh data sources
+"""Implement consumer classes that provide Acoular data to Bokeh data sources.
 
 .. autosummary::
     :toctree: generated/
@@ -11,35 +11,37 @@
 """
 
 import threading
-from numpy import arange
+
+import numpy as np
 from acoular import TimeOut
 from bokeh.models import ColumnDataSource
 from traits.api import (
-    List,
-    Int,
-    Trait,
-    Float,
     Array,
-    Property,
-    on_trait_change,
-    cached_property,
     Callable,
+    Float,
+    Int,
+    List,
+    Property,
+    Trait,
+    cached_property,
+    on_trait_change,
 )
 
 
 class TimeConsumer(TimeOut):
-    """
-    :class:`TimeOut` derived class for building an interface from Acoular's generator
-    pipelines to Bokeh's ColumnDataSource model that serves as a source for
-    plots and tables.
+    """Provide Acoular time data to a Bokeh ``ColumnDataSource``.
 
-    How it works:
-    :meth:`consume` runs in an extra non-GUI :attr:`thread` (must be set up by the user)
-    and fetches blocks of data  with length :attr:`num` from :attr:`source`.
-    These blocks are stored in :attr:`data` and :meth:`update` is then registered
-    as a callback for GUI event loop. :meth:`update` then sends the data, downsampled
-    by a factor :attr:`down` to ColumnDataSource :meth:`ds`, which holds an overall
-    length of :attr:`rollover` samples. The elapsed time in seconds is stored in :attr:`elapsed`
+    This :class:`TimeOut`-derived class builds an interface from Acoular's
+    generator pipelines to Bokeh's ``ColumnDataSource`` model for plots and
+    tables.
+
+    :meth:`consume` runs in an extra non-GUI :attr:`thread` and fetches blocks
+    of data with length :attr:`num` from :attr:`source`. These blocks are stored
+    in :attr:`data`, and :meth:`update` is then registered as a callback for the
+    GUI event loop. :meth:`update` sends the data, downsampled by a factor
+    :attr:`down`, to :attr:`ds`, which holds an overall length of
+    :attr:`rollover` samples. The elapsed time in seconds is stored in
+    :attr:`elapsed`.
     """
 
     #: Bokeh's ColumnDataSource, updated from result loop
@@ -69,10 +71,11 @@ class TimeConsumer(TimeOut):
     #: transport between consume / update
     data = Array
 
-    @on_trait_change("channels,source.num_channels")
+    @on_trait_change('channels,source.num_channels')
     def init_ds(self):
+        """Initialize the ``ColumnDataSource`` with channel columns."""
         data = {}
-        data["t"] = []
+        data['t'] = []
         for ch in self.ch_names():
             data[ch] = []
         if not self.ds:
@@ -80,26 +83,26 @@ class TimeConsumer(TimeOut):
         self.ds.data = data  # ColumnDataSource wants all columns at once
 
     def ch_names(self):
-        """
-        generator that returns channel names, helper for iterating over channels
-        """
+        """Yield channel names for iterating over channels."""
         chan = self.channels
         for ch in chan:
-            yield "timedata{}".format(ch)
+            yield f'timedata{ch}'
 
     def consume(self, doc):
-        """
-        consumes samples from source, to be run in an extra non-GUI thread
+        """Consume samples from the source in a non-GUI thread.
 
-        Args:
-            doc: server document to register callbacks to
+        Parameters
+        ----------
+        doc
+            Server document used to register callbacks.
+
         """
         self.elapsed = 0.0
         self.updated = threading.Event()
         self.updated.set()
         doc.add_next_tick_callback(self.init_ds)
         for temp in self.source.result(self.num):
-            if not getattr(self.thread, "do_run", True):
+            if not getattr(self.thread, 'do_run', True):
                 break
             self.updated.wait()
             self.data = temp
@@ -107,60 +110,58 @@ class TimeConsumer(TimeOut):
             doc.add_next_tick_callback(self.update)
 
     def update(self):
-        """callback function for GUI event loop, updates data source"""
+        """Update the data source from the GUI event loop."""
         if not self.updated.is_set():
             newdata = {}
-            newdata["t"] = (
-                self.down * arange(self.num / self.down) / self.sample_freq
-                + self.elapsed
-            )
+            newdata['t'] = self.down * np.arange(self.num / self.down) / self.sample_freq + self.elapsed
             self.elapsed += self.num / self.sample_freq
-            for i, ch in zip(self.channels, self.ch_names()):
+            for i, ch in zip(self.channels, self.ch_names(), strict=False):
                 newdata[ch] = self.data[:: self.down, i]
             self.ds.stream(newdata, rollover=self.rollover)
             self.updated.set()
 
     def result(self, num):
+        """Yield the output block-wise.
+
+        This method does nothing in this class.
+
         """
-        Python generator that yields the output block-wise. Does nothing in this class.
-        """
-        pass
 
 
 class TimeBandsConsumer(TimeConsumer):
-    """
-    :class:`TimeConsumer` derived class for building an interface from Acoular's generator
-    pipelines to Bokeh's ColumnDataSource which specialises in Plots over frequency bands
-    (like octave spectrum)
+    """Provide frequency-band data to a Bokeh ``ColumnDataSource``.
 
-    Works like :class:`TimeConsumer`, but ignores :attr:`down` and :attr:`rollover`. Instead,
-    just the first sample of each block is taken from the input. :attr:`ds` has a column for
-    each input channel with one frequency band value in each row. :attr:`bands` gives the bands
-    and :attr:`lfunc` can be used to convert to human-readable label strings.
+    This :class:`TimeConsumer`-derived class specializes in plots over
+    frequency bands, such as octave spectra.
+
+    It works like :class:`TimeConsumer`, but ignores :attr:`down` and
+    :attr:`rollover`. Instead, only the first sample of each block is taken
+    from the input. :attr:`ds` has a column for each input channel with one
+    frequency-band value in each row. :attr:`bands` gives the bands, and
+    :attr:`lfunc` can be used to convert them to human-readable label strings.
     """
 
     #: Function to convert bands into list of labels
-    lfunc = Callable(lambda bands: ["{:.0f}".format(f) for f in bands])
+    lfunc = Callable(lambda bands: [f'{f:.0f}' for f in bands])
 
     #: List of labels for bands
-    bands = Property(depends_on=["source.digest"])
+    bands = Property(depends_on=['source.digest'])
 
     #: Number of bands
-    numbands = Property(depends_on=["source.digest"])
+    numbands = Property(depends_on=['source.digest'])
 
     @cached_property
     def _get_bands(self):
         obj = self.source  # start with source
         bands = [1]  # if no FilterBank or similar is found
         while obj:
-            if "bands" in obj.all_trait_names():  # at FilterBank?
+            if 'bands' in obj.all_trait_names():  # at FilterBank?
                 bands = obj.bands  # get the name
                 break
-            else:
-                try:
-                    obj = obj.source  # traverse down until FilterBank
-                except AttributeError:
-                    obj = None
+            try:
+                obj = obj.source  # traverse down until FilterBank
+            except AttributeError:
+                obj = None
         return bands
 
     @cached_property
@@ -168,24 +169,23 @@ class TimeBandsConsumer(TimeConsumer):
         obj = self.source  # start with source
         numbands = 1  # if no FilterBank or similar is found
         while obj:
-            if "numbands" in obj.all_trait_names():  # at FilterBank?
+            if 'numbands' in obj.all_trait_names():  # at FilterBank?
                 numbands = obj.numbands  # get the name
                 break
-            else:
-                try:
-                    obj = obj.source  # traverse down until FilterBank
-                except AttributeError:
-                    obj = None
+            try:
+                obj = obj.source  # traverse down until FilterBank
+            except AttributeError:
+                obj = None
         return numbands
 
     def update(self):
-        """callback function for GUI event loop, updates data source"""
+        """Update the data source from the GUI event loop."""
         if not self.updated.is_set():
             newdata = {}
-            newdata["t"] = self.lfunc(self.bands)
+            newdata['t'] = self.lfunc(self.bands)
             self.elapsed += self.num / self.sample_freq
             data = self.data[0].reshape((self.numbands, -1))
-            for i, ch in zip(self.channels, self.ch_names()):
+            for i, ch in zip(self.channels, self.ch_names(), strict=False):
                 newdata[ch] = data[:, i]
             self.ds.data = newdata
             self.updated.set()
