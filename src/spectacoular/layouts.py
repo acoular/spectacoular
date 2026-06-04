d657d1ae2490c409479f0ab2367a69f5ae386528
jtodev <jan@ostendorf.com>
2026-06-04 19:38:41 +0200

    Format layouts

--- src/spectacoular/layouts.py
+++ src/spectacoular/layouts.py
@@ -1,34 +1,38 @@
-"""
-Ready to use layout components that provide a high-level way to visualize and interact with Acoular.
-"""
+"""Provide ready-to-use layout components for Acoular visualization."""
+
+from pathlib import Path
 
 import acoular as ac
-from pathlib import Path
 import bokeh
+import numpy as np
 from bokeh.plotting import figure
 from traits.api import (
-    Instance,
-    observe,
-    Property,
     Bool,
-    cached_property,
     Dict,
-    Int,
     Float,
+    Instance,
+    Int,
+    Property,
+    cached_property,
+    observe,
 )
+
 from .dprocess import MicGeomPresenter
-from .factory import get_widgets, BaseSpectacoular
-import numpy as np
+from .factory import BaseSpectacoular, get_widgets
 
 
 def get_mic_files():
-    """
-    Get a list of all available microphone geometry files.
-    """
-    return [str(file) for file in (Path(ac.__file__).parent / "xml").glob("*")]
+    """Get a list of all available microphone geometry files."""
+    return [str(file) for file in (Path(ac.__file__).parent / 'xml').glob('*')]
 
 
 class MicGeomComponent(BaseSpectacoular):
+    """Combine microphone-geometry plotting and widget controls.
+
+    This component bundles a :class:`MicGeomPresenter`, a Bokeh figure, and a
+    set of widgets for interacting with microphone geometry data.
+    """
+
     presenter = Instance(MicGeomPresenter)
 
     figure = Instance(figure)
@@ -37,11 +41,11 @@ class MicGeomComponent(BaseSpectacoular):
     glyph = Instance(
         bokeh.models.glyphs.Scatter,
         kw={
-            "marker": "circle_cross",
-            "x": "x",
-            "y": "y",
-            "size": 20,
-            "fill_alpha": "alpha",
+            'marker': 'circle_cross',
+            'x': 'x',
+            'y': 'y',
+            'size': 20,
+            'fill_alpha': 'alpha',
         },
     )
 
@@ -59,8 +63,8 @@ class MicGeomComponent(BaseSpectacoular):
     #: a Bokeh widget. Keys: name of the trait attribute. Values: Bokeh widget.
     trait_widget_mapper = Dict(
         {
-            "mirror_view": bokeh.models.widgets.Toggle,
-            "mic_size": bokeh.models.widgets.Slider,
+            'mirror_view': bokeh.models.widgets.Toggle,
+            'mic_size': bokeh.models.widgets.Slider,
         }
     )
 
@@ -69,23 +73,23 @@ class MicGeomComponent(BaseSpectacoular):
     #: For example: {"traitname":{'disabled':True,'background_color':'red',...}}.
     trait_widget_args = Dict(
         {
-            "mirror_view": {"label": "Mirror X-Axis", "active": False},
-            "mic_size": {"title": "Mic Size", "start": 0, "end": 50},
+            'mirror_view': {'label': 'Mirror X-Axis', 'active': False},
+            'mic_size': {'title': 'Mic Size', 'start': 0, 'end': 50},
         }
     )
 
     #: dictionary containing the mapping between a class trait attribute and
     #: a Bokeh widget for the microphone geometry object.
-    mics_trait_widget_mapper = Dict({"file": bokeh.models.widgets.Select})
+    mics_trait_widget_mapper = Dict({'file': bokeh.models.widgets.Select})
 
     #: dictionary containing arguments that belongs to a widget that is created
     #: from a trait attribute and should be considered when the widget is built.
     mics_trait_widget_args = Dict(
         {
-            "file": {
-                "title": "Geometry File",
-                "value": get_mic_files()[0],
-                "options": get_mic_files(),
+            'file': {
+                'title': 'Geometry File',
+                'value': get_mic_files()[0],
+                'options': get_mic_files(),
             },
         }
     )
@@ -93,7 +97,8 @@ class MicGeomComponent(BaseSpectacoular):
     _glyph_renderer = Instance(bokeh.models.renderers.GlyphRenderer)
 
     def _mirror_x_axis(self, event):
-        """Flips the x-axis view of the scatter plot when the button is toggled."""
+        """Flip the x-axis view of the scatter plot when the button is toggled."""
+        del event
         if self.figure:
             current_start, current_end = (
                 self.figure.x_range.start,
@@ -105,42 +110,37 @@ class MicGeomComponent(BaseSpectacoular):
             )
 
     def update_mic_size(self, attr, old, new):
-        if (
-            type(self.glyph.size) is not str
+        """Update microphone glyph sizes in the plot and data source."""
+        del attr, old, new
+        if not isinstance(
+            self.glyph.size, str
         ):  # if size is a string, it is a reference to a column in the data source
             self.glyph.size = self.mic_size
-        self.presenter.cdsource.data["sizes"] = (
-            np.ones(self.presenter.source.pos_total.shape[1]) * self.mic_size
-        )
+        self.presenter.cdsource.data['sizes'] = np.ones(self.presenter.source.pos_total.shape[1]) * self.mic_size
 
-    @observe("figure")
+    @observe('figure')
     def _remove_logo(self, event):
+        del event
         self.figure.toolbar.logo = None
 
-    @observe("presenter.cdsource, figure")
+    @observe('presenter.cdsource, figure')
     def _add_glyph(self, event):
-        if hasattr(self.presenter, "cdsource") and self.figure:
+        del event
+        if hasattr(self.presenter, 'cdsource') and self.figure:
             # Remove previous glyph if it exists
             if self._glyph_renderer in self.figure.renderers:
                 self.figure.renderers.remove(self._glyph_renderer)
-            self._glyph_renderer = self.figure.add_glyph(
-                self.presenter.cdsource, self.glyph
-            )
+            self._glyph_renderer = self.figure.add_glyph(self.presenter.cdsource, self.glyph)
 
-    @observe("figure, _glyph_renderer, allow_point_draw")
+    @observe('figure, _glyph_renderer, allow_point_draw')
     def _add_point_draw(self, event):
-        existing_tools = [
-            tool
-            for tool in self.figure.tools
-            if isinstance(tool, bokeh.models.PointDrawTool)
-        ]
+        del event
+        existing_tools = [tool for tool in self.figure.tools if isinstance(tool, bokeh.models.PointDrawTool)]
         for tool in existing_tools:
             self.figure.tools.remove(tool)
 
         if self.allow_point_draw and self._glyph_renderer is not None:
-            draw_tool = bokeh.models.PointDrawTool(
-                renderers=[self._glyph_renderer], empty_value=0.0
-            )
+            draw_tool = bokeh.models.PointDrawTool(renderers=[self._glyph_renderer], empty_value=0.0)
             self.figure.add_tools(draw_tool)
             self.figure.toolbar.active_tap = draw_tool
 
@@ -152,33 +152,30 @@ class MicGeomComponent(BaseSpectacoular):
             alpha[inv] = 0
             widget.options = [str(i) for i in range(nmics)]
             widget.value = [str(i) for i in inv]
-            self.presenter.cdsource.data["alpha"] = alpha
+            self.presenter.cdsource.data['alpha'] = alpha
 
         _update_invalid_channels()
         if self.presenter is not None:
-            self.presenter.source.on_trait_change(_update_invalid_channels, "digest")
+            self.presenter.source.on_trait_change(_update_invalid_channels, 'digest')
 
     @cached_property
     def _get_widgets(self):
         # component widgets
         widgets = []
         cwidgets = self.get_widgets()
-        if "mirror_view" in cwidgets.keys():
-            cwidgets["mirror_view"].on_click(self._mirror_x_axis)
-        if "mic_size" in cwidgets.keys():
-            cwidgets["mic_size"].on_change("value", self.update_mic_size)
+        if 'mirror_view' in cwidgets:
+            cwidgets['mirror_view'].on_click(self._mirror_x_axis)
+        if 'mic_size' in cwidgets:
+            cwidgets['mic_size'].on_change('value', self.update_mic_size)
 
         if self.presenter is not None:
-            if not hasattr(self.presenter.source, "get_widgets"):
-                if "pos_total" in self.mics_trait_widget_mapper.keys():
-                    if (
-                        self.mics_trait_widget_args.get("pos_total") is None
-                    ) or self.mics_trait_widget_args["pos_total"].get(
-                        "source"
-                    ) is not None:
-                        self.mics_trait_widget_args["pos_total"].update(
-                            {"source": self.presenter.cdsource}
-                        )
+            if not hasattr(self.presenter.source, 'get_widgets'):
+                if (
+                    'pos_total' in self.mics_trait_widget_mapper
+                    and self.mics_trait_widget_args.get('pos_total') is not None
+                    and self.mics_trait_widget_args['pos_total'].get('source') is not None
+                ):
+                    self.mics_trait_widget_args['pos_total'].update({'source': self.presenter.cdsource})
                 widgets = get_widgets(
                     self.presenter.source,
                     self.mics_trait_widget_mapper,
@@ -186,25 +183,18 @@ class MicGeomComponent(BaseSpectacoular):
                 )
             else:
                 # update widget mapper
-                self.presenter.source.trait_widget_mapper.update(
-                    self.mics_trait_widget_mapper
-                )
-                self.presenter.source.trait_widget_args.update(
-                    self.mics_trait_widget_args
-                )
-                if "pos_total" in self.presenter.source.trait_widget_mapper.keys():
-                    if (
-                        self.presenter.source.trait_widget_args.get("pos_total") is None
-                    ) or self.presenter.source.trait_widget_args["pos_total"].get(
-                        "source"
-                    ) is None:
-                        self.presenter.source.trait_widget_args["pos_total"].update(
-                            {"source": self.presenter.cdsource}
-                        )
+                self.presenter.source.trait_widget_mapper.update(self.mics_trait_widget_mapper)
+                self.presenter.source.trait_widget_args.update(self.mics_trait_widget_args)
+                if (
+                    'pos_total' in self.presenter.source.trait_widget_mapper
+                    and self.presenter.source.trait_widget_args.get('pos_total') is not None
+                    and self.presenter.source.trait_widget_args['pos_total'].get('source') is None
+                ):
+                    self.presenter.source.trait_widget_args['pos_total'].update({'source': self.presenter.cdsource})
                 widgets = self.presenter.source.get_widgets()
-        if widgets.get("invalid_channels"):
-            self._add_invalid_channel_callback(widgets["invalid_channels"])
+        if widgets.get('invalid_channels'):
+            self._add_invalid_channel_callback(widgets['invalid_channels'])
         cwidgets.update(widgets)
-        if widgets.get("file"):
-            widgets["file"].on_change("value", self.update_mic_size)
+        if widgets.get('file'):
+            widgets['file'].on_change('value', self.update_mic_size)
         return cwidgets
