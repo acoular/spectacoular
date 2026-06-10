64ef37caa61b92958e52b326a67a94db724f47eb
jtodev <jan@ostendorf.com>
2026-06-10 14:46:44 +0200

    Format test/test_get_widget.py

--- tests/test_get_widgets.py
+++ tests/test_get_widgets.py
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
 # Copyright (c) Acoular Development Team.
 # ------------------------------------------------------------------------------
+"""Tests for widget generation on SpectAcoular-exposed classes."""
 
 import importlib
 import inspect
@@ -9,10 +10,12 @@ import pkgutil
 import spectacoular as sp
 
 import pytest
+import sounddevice as sd
 from traits.api import HasTraits
 
 
-def get_all_classes(hastraits_only=False, module='acoular'):
+def get_all_classes(*, hastraits_only=False, module='acoular'):
+    """Collect classes from a package and, optionally, only ``HasTraits`` subclasses."""
     classes = []
     package = importlib.import_module(module)
     for module_info in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
@@ -33,26 +36,28 @@ acoular_classes = get_all_classes()
 spectacoular_classes = get_all_classes(hastraits_only=True, module='spectacoular')
 spectacoular_class_names = [cls.__name__ for cls in spectacoular_classes]
 
-if False:  # TODO: must be fixed in a future release. Commented out for now to pass CI
 
-    @pytest.mark.parametrize('cls', acoular_classes)
-    def test_view_exists(cls):
-        """Tests that for each class in Acoular a corresponding object in Acoular exists."""
-        # skip abstract base classes
-        if inspect.isabstract(cls):
-            pytest.skip(f'{cls} is an abstract base class.')
-        # test if class can be imported from SpectAcoular and has get_widgets method and trait_widget_mapper
-        sp_cls = getattr(sp, cls.__name__, None)
-        assert hasattr(sp_cls, 'get_widgets')
-        assert hasattr(sp_cls, 'trait_widget_mapper')
+@pytest.mark.skip(reason='Not all Acoular classes have a validated SpectAcoular view yet.')
+@pytest.mark.parametrize('cls', acoular_classes)
+def test_view_exists(cls):
+    """Check that an Acoular class is exposed with widget metadata in SpectAcoular."""
+    if inspect.isabstract(cls):
+        pytest.skip(f'{cls} is an abstract base class.')
+    sp_cls = getattr(sp, cls.__name__, None)
+    if sp_cls is None:
+        pytest.fail(f'{cls.__name__} is not exposed by SpectAcoular.')
+    if not hasattr(sp_cls, 'get_widgets'):
+        pytest.fail(f'{cls.__name__} does not define get_widgets.')
+    if not hasattr(sp_cls, 'trait_widget_mapper'):
+        pytest.fail(f'{cls.__name__} does not define trait_widget_mapper.')
 
 
 @pytest.mark.parametrize('cls', acoular_classes)
 def test_get_widgets(cls):
-    """Test that get_widgets can be called"""
+    """Test that ``get_widgets`` can be called."""
     if inspect.isabstract(cls):
         pytest.skip(f'{cls} is an abstract base class.')
-    # test if class can be imported from SpectAcoular and has get_widgets method and trait_widget_mapper
+    # Test whether the class is exposed by SpectAcoular and provides widget mappings.
     sp_cls = getattr(sp, cls.__name__, None)
     if sp_cls is None:
         pytest.skip(f'{cls} is not implemented in SpectAcoular.')
@@ -60,9 +65,8 @@ def test_get_widgets(cls):
         pytest.skip(f'{cls} does not have get_widgets method.')
 
     if cls.__name__ == 'SoundDeviceSamplesGenerator':
-        import sounddevice as sd
-
-        # in some cases, the default value of device=0 is not valid. Thus, we use the default input device here
+        # In some cases, the default value ``device=0`` is invalid.
+        # Use the default input device instead.
         device = sd.default.device[0]
         try:
             sd.query_devices(device)
@@ -73,7 +77,8 @@ def test_get_widgets(cls):
         # test default
         sp_instance = sp_cls()
     widgets_default = sp_instance.get_widgets()
-    assert len(widgets_default) > 0, f'get_widgets returned an empty dict for {cls.__name__} with default mapping'
+    if len(widgets_default) == 0:
+        pytest.fail(f'get_widgets returned an empty dict for {cls.__name__} with default mapping')
 
     # now test each default mapping individually
     for key, value in sp_instance.trait_widget_mapper.items():
