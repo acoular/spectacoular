3a1f2a89974ae298ba4ed5cfe581e49cd39e35c6
jtodev <jan@ostendorf.com>
2026-06-13 00:39:31 +0200

    Format acoular_future/spectra

--- src/spectacoular/apps/measurement_app/acoular_future/spectra.py
+++ src/spectacoular/apps/measurement_app/acoular_future/spectra.py
@@ -1,21 +1,25 @@
+"""Spectra helpers for measurement-app Acoular extensions."""
+
 # ------------------------------------------------------------------------------
 # Copyright (c) Acoular Development Team.
 # ------------------------------------------------------------------------------
 import acoular as ac
-import numpy as np
 from spectacoular import BaseSpectacoular
+
+from .fastFuncs import calc_csmmav
+
+import numpy as np
 from traits.api import (
-    Int,
-    Property,
-    Trait,
-    Range,
     Bool,
-    cached_property,
-    property_depends_on,
     CArray,
     Float,
+    Int,
+    Property,
+    Range,
+    Trait,
+    cached_property,
+    property_depends_on,
 )
-from .fastFuncs import calcCSMmav
 
 
 class CSMInOut(ac.SpectraOut, BaseSpectacoular):
@@ -39,21 +43,21 @@ class CSMInOut(ac.SpectraOut, BaseSpectacoular):
         4096,
         8192,
         16384,
-        desc="number of samples per FFT block",
+        desc='number of samples per FFT block',
     )
 
     #: Index of lowest frequency line to compute, integer, defaults to 1,
     #: is used only by objects that fetch the csm, PowerSpectra computes every
     #: frequency line.
-    ind_low = Range(1, desc="index of lowest frequency line")
+    ind_low = Range(1, desc='index of lowest frequency line')
 
     #: Index of highest frequency line to compute, integer,
     #: defaults to -1 (last possible line for default block_size).
-    ind_high = Int(-1, desc="index of highest frequency line")
+    ind_high = Int(-1, desc='index of highest frequency line')
 
     #: Array with a sequence of indices for all frequencies
     #: between :attr:`ind_low` and :attr:`ind_high` within the result, readonly.
-    indices = Property(desc="index range")
+    indices = Property(desc='index range')
 
     band_width = Int(0)
 
@@ -62,27 +66,21 @@ class CSMInOut(ac.SpectraOut, BaseSpectacoular):
     # SPL time weighting : SLOW: 1.0, FAST: 0.125 (default), arbitrary values possible
     weight_time = Float(0.125)
 
-    #: Flag, if tru (default), current csm will be calculated with cumulated average of past CSMs
-    accumulate = Bool(False, desc="CSM accumulation flag")
+    #: Flag indicating whether the current CSM is accumulated over past CSMs.
+    accumulate = Bool(default_value=False, desc='CSM accumulation flag')
 
-    @property_depends_on("block_size, ind_low, ind_high, band_width")
+    @property_depends_on('block_size, ind_low, ind_high, band_width')
     def _get_indices(self):
         try:
             if self.band_width == 0:
-                return np.arange(self.block_size / 2 + 1, dtype=int)[
-                    self.ind_low : self.ind_high
-                ]
-            else:
-                return np.array([0])
+                return np.arange(self.block_size / 2 + 1, dtype=int)[self.ind_low : self.ind_high]
+            return np.array([0])
         except IndexError:
             return range(0)
 
     def fftfreq_fine(self):
-        return abs(
-            np.fft.fftfreq(self.block_size, 1.0 / self.sample_freq)[
-                : int(self.block_size / 2 + 1)
-            ]
-        )
+        """Return the full FFT frequency grid for the configured block size."""
+        return abs(np.fft.fftfreq(self.block_size, 1.0 / self.sample_freq)[: int(self.block_size / 2 + 1)])
 
     def fftfreq(self):
         """
@@ -95,8 +93,7 @@ class CSMInOut(ac.SpectraOut, BaseSpectacoular):
         """
         if self.band_width == 0:
             return self.fftfreq_fine()
-        else:
-            return np.array([self.center_freq])
+        return np.array([self.center_freq])
 
     def result(self, num=1):
         """
@@ -119,38 +116,33 @@ class CSMInOut(ac.SpectraOut, BaseSpectacoular):
         # init csm
         numfreq = bs // 2 + 1
         csm_shape = (numfreq, self.source.num_channels, self.source.num_channels)
-        csmBlock = np.zeros(csm_shape, dtype=self.precision)
+        csm_block = np.zeros(csm_shape, dtype=self.precision)
         # calc the weight function
         wind = self.window_(bs)
         block_weight = np.dot(wind, wind)
         wind = wind[:, np.newaxis]
         # upper diagonal
-        csmUpper = np.zeros(csm_shape, dtype=self.precision)
+        csm_upper = np.zeros(csm_shape, dtype=self.precision)
         # dsm dummy needed for single band workaround
-        block = 0
         alpha = 1.0  # initial value
         numblocks = 1  # num
-        for temp in self.source.result(bs):
+        for block, temp in enumerate(self.source.result(bs), start=1):
             ft = np.fft.rfft(temp * wind, None, 0).astype(self.precision)
             # calc csm
-            calcCSMmav(csmUpper, ft, alpha)  # calcCSM(csmUpper, ft)
-
-            block += 1
+            calc_csmmav(csm_upper, ft, alpha)  # calcCSM(csm_upper, ft)
 
             if block % num == 0:
                 # put together
-                csmLower = csmUpper.conj().transpose(0, 2, 1)
-                [
-                    np.fill_diagonal(csmLower[cntFreq, :, :], 0)
-                    for cntFreq in range(csmLower.shape[0])
-                ]
+                csm_lower = csm_upper.conj().transpose(0, 2, 1)
+                for cnt_freq in range(csm_lower.shape[0]):
+                    np.fill_diagonal(csm_lower[cnt_freq, :, :], 0)
                 # fill csm
-                csmBlock = csmLower + csmUpper
+                csm_block = csm_lower + csm_upper
                 if self.band_width == 0:
-                    yield csmBlock * (2.0 / bs / block_weight)
+                    yield csm_block * (2.0 / bs / block_weight)
                 else:
                     yield ac.synthetic(
-                        csmBlock * (2.0 / bs / block_weight),
+                        csm_block * (2.0 / bs / block_weight),
                         self.fftfreq_fine(),
                         self.center_freq,
                         self.band_width,
@@ -166,30 +158,28 @@ class CSMInOut(ac.SpectraOut, BaseSpectacoular):
 
 
 class PowerSpectraSetCSM(ac.PowerSpectra):
-    """
-    currently only helper class for BeamformerFreqTime
-    """
+    """Helper class for BeamformerFreqTime."""
 
     _fftfreq = CArray()
 
     csm = CArray(dtype=complex)
 
     #: Array with a sequence of indices for all frequencies
-    indices = CArray(desc="index range")
+    indices = CArray(desc='index range')
 
     #: Sampling frequency of the signal, defaults to 1.0
-    sample_freq = Float(1.0, desc="sampling frequency")
+    sample_freq = Float(1.0, desc='sampling frequency')
 
     num_channels = Property()
 
     cached = False
 
     #: Basename dummy
-    basename = "dummy"
+    basename = 'dummy'
 
     # internal identifier
     digest = Property(
-        depends_on=["csm", "sample_freq"],
+        depends_on=['csm', 'sample_freq'],
     )
 
     def _get_num_channels(self):
